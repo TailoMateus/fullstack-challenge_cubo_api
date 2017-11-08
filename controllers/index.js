7b11f677c7e82924564d30abb8a56bbce7da1e0e
TailoMateus <tailogonsalves@gmail.com>
2017-11-08 11:54:10 -0200

    Add methods HTTP

--- controllers/index.js
+++ controllers/index.js
@@ -4,7 +4,65 @@ module.exports = function(app) {
 		res.send('OK');
 	});
 
+	app.delete('/index/dados/:id', function(req, res) {
+		var id = req.params.id;
+		var dados = {};
+
+		dados.id = id;
+		dados.status = 'CANCELADO';
+
+		var connection = app.persistencia.connectionFactory();
+		var dadosDao = new app.persistencia.DadosDao(connection);
+		
+		dadosDao.atualiza(dados, function(erro){
+			if(erro) {
+				res.status(500).send(erro);
+				return;
+			}
+
+			res.status(204).send(dados);
+		});
+	});
+
+	app.put('/index/dados/:id', function(req, res) {
+		var id = req.params.id;
+		var dados = {};
+
+		dados.id = id;
+		dados.status = 'CONFIRMADO';
+
+		var connection = app.persistencia.connectionFactory();
+		var dadosDao = new app.persistencia.DadosDao(connection);
+		
+		dadosDao.atualiza(dados, function(erro){
+			if(erro) {
+				res.status(500).send(erro);
+				return;
+			}
+
+			res.send(dados);
+		});
+	}
+
 	app.post('/index/dados', function(req, res){
+		
+		req.assert("nome", 
+			"Nome eh obrigatorio").notEmpty();
+
+		req.assert("sobrenome", 
+			"Sobrenome eh obrigatorio").notEmpty();
+
+		req.assert("participacao", 
+			"Participacao eh obrigatorio").notEmpty();
+
+		var erros = req.validationErrors();
+
+		if(erros) {
+			console.log("Erros de validacao");
+			res.status(400).send(erros);
+			return;
+		}
+
 		var dados = req.body;
 
 		dados.status = 'CRIADO';
@@ -14,8 +72,30 @@ module.exports = function(app) {
 		var dadosDao = new app.persistencia.DadosDao(connection);
 
 		dadosDao.salva(dados, function(erro, resultado){
-			console.log('Dados Criado');
-			res.json(dados);
+			
+			dados.id = resultado.insertId;
+
+			res.location('/index/dados/' + dados.id);
+
+			if(erro) {
+				res.status(500).send(erros);
+			}
+			else {
+				console.log('Dados Criado');
+
+				var response = {
+					dados_da_pessoa: dados,
+					links: [
+						{
+							href:"http://localhost:3000/index/dados"+dados.id,
+							rel:"confirmar",
+							method:"PUT"
+						}
+					]
+				}
+				
+				res.status(201).json(response);
+			}
 		});
 	})
 }
--- persistencia/DadosDao.js
+++ persistencia/DadosDao.js
@@ -6,6 +6,10 @@ DadosDao.prototype.salva = function(dados,callback) {
     this._connection.query('INSERT INTO dados SET ?', dados, callback);
 }
 
+DadosDao.prototype.atualiza = function(dados,callback) {
+    this._connection.query('UPDATE dados SET status = ? where id = ?', [dados.status, dados.id], callback);
+}
+
 DadosDao.prototype.lista = function(callback) {
     this._connection.query('select * from dados',callback);
 }
